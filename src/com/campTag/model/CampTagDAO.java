ebf47dc7e7a5b713eb79bc59edf79f3c14f6c69a
alicekuokuo <st0424alice@gmail.com>
2021-11-25 09:13:17 +0800

    新增favoritecamp,camptagservice

--- src/com/campTag/model/CampTagDAO.java
+++ src/com/campTag/model/CampTagDAO.java
@@ -1,13 +1,20 @@
 package com.campTag.model;
 
+import java.util.List;
+
 public interface CampTagDAO {
 
-	void insert(CampTagVO campTagVO);
+	void add(CampTagVO campTagVO);
 
 	CampTagVO findByPrimaryKey(Integer campTagId);
 
 	void delete(Integer campTagId);
 
 	void update(CampTagVO campTagVO);
+	
+	List<CampTagVO> getAll();
+	
+	List<CampTagVO> getAllTag();
+	
 
 }
---
+++ src/com/campTag/model/CampTagService.java
@@ -0,0 +1,57 @@
+package com.campTag.model;
+
+import java.util.List;
+
+public class CampTagService {
+
+	private  CampTagDAO  camptagdao;
+	
+	public CampTagService() {
+		camptagdao = new  CampTagDAOlmpl();
+	}
+
+	//新增營地標籤
+	public  void  addCampTag(Integer campTagId, String campTagName) {
+
+		 CampTagVO  campTagVO = new  CampTagVO();
+		 
+		 campTagVO.setCampTagId(campTagId);		 
+		 campTagVO.setCampTagName(campTagName);
+		  camptagdao.add(campTagVO);
+	 
+
+	}
+	
+	//刪除營地標籤
+	public void deleteCampTag(Integer campTagId) {
+		 camptagdao.delete(campTagId);
+	}
+	
+	
+	//更新營地標籤
+	public void  updateCampTag(Integer campTagId, String campTagName) {
+		 
+		 CampTagVO  campTagVO = new  CampTagVO();
+		 
+		 campTagVO.setCampTagId(campTagId);		 
+		 campTagVO.setCampTagName(campTagName);
+	}
+		 
+
+	
+	//查詢某個/營地標籤
+	public CampTagVO getOneEmp(Integer campTagId) {
+		return camptagdao.findByPrimaryKey(campTagId);
+	}                                          
+	
+	//查詢全部營地標籤
+	public List<CampTagVO> getAll() {
+		return  camptagdao.getAll();
+	}
+}
+	
+	
+	
+	
+	
+
